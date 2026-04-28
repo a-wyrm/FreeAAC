5ecbb310e5bc3cd7f90d18c5821a15b84c82b3f4
Chris Baume <chris@bau.me>
2026-04-28 19:21:55 +0100

    Don't animate stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -77,6 +77,7 @@ export default function RootLayout() {
             <GestureHandlerRootView>
               <Stack
                 screenOptions={{
+                  animation: "none",
                   headerBackButtonDisplayMode: "minimal",
                 }}
               >
