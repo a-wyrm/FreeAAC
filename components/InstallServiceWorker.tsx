c7679a4dd9cce8a21fb034c4a4f43118517aaefe
Chris Baume <chris@bau.me>
2026-06-03 14:08:16 +0100

    Disabled updateViaCache for service worker

--- components/InstallServiceWorker.tsx
+++ components/InstallServiceWorker.tsx
@@ -3,7 +3,7 @@ import { Platform } from "react-native"
 const sw = `
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./sw.js').then(registration => {
+        navigator.serviceWorker.register('./sw.js', { updateViaCache: 'none' }).then(registration => {
             console.log('Service Worker registered with scope:', registration.scope);
         }).catch(error => {
             console.error('Service Worker registration failed:', error);
